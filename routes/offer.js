7366b4f90720a9a81dee3537e099f9b46716c6a3
SEGURA Nassim <seguranassim@air-de-segura.home>
2023-08-06 19:57:01 +0200

    correction of .env name

--- routes/offer.js
+++ routes/offer.js
@@ -9,7 +9,7 @@ const User = require("../Models/User");
 const router = express.Router();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
